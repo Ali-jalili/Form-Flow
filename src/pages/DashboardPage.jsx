46d7c6ee01f49d31a4774f65c9b89f64e026095b
Ali Jalili <mrali97jl@gmail.com>
2026-06-07 14:49:37 +0330

    feat(dashboard): add loading, error, and empty state handling

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -4,15 +4,20 @@ import useForms from "../features/form-builder/hooks/useForms";
 import Spinner from "../ui/Spinner";
 
 function DashboardPage() {
-  const { data, isLoading, error } = useForms;
+  const { data, isLoading, error } = useForms();
 
-  return (
-    <div>
-      {isLoading && <Spinner />}
+  if (isLoading) return <Spinner />;
+  if (error) return <div>{error.message}</div>;
+  if (data.length === 0)
+    return (
+      <div>
+        <h2> No forms yet</h2>
+        <p>Create your first form and start collecting responses</p>
+        <button> {} </button>
+      </div>
+    );
 
-      <h1>DashboardPage</h1>
-    </div>
-  );
+  return <div>لیست فرم‌ها</div>;
 }
 
 export default DashboardPage;
